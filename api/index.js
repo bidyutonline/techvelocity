e6676dee858b8cc57b1d206e2cf126ba836521ce
Bidyut Mondal <bidyutonline@gmail.com>
2024-02-01 10:40:36 +0530

    Increased payload limit in server

--- api/index.js
+++ api/index.js
@@ -4,7 +4,8 @@ const app = express()
 
 const tutorialsRoutes = require('./tutorials')
 
-app.use(express.json())
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use(cors())
 
 app.use('/api/tutorials', tutorialsRoutes)
